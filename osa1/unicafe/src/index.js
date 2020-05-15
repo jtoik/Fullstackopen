45d8ceeb32bd35b82b1634b4c33384df94c72bbe
jtoik <jeretoikkadev@gmail.com>
2020-05-15 21:28:27 +0300

    Tehtävä 1.8

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Display = props => <div>{props.name} {props.value}</div>
+const Display = props => <div>{props.good} {props.good}</div>
 
 const Button = (props) => (
   <button onClick={props.handleClick}>
@@ -33,6 +33,21 @@ const Percentage = (props) => {
   )
 }
 
+const Statistics = (props) => {
+  return(
+    <div>
+      <h1>Stats</h1>
+      <Display name='Good:' value={props.good} />
+      <Display name='Neutral:' value={props.neutral} />
+      <Display name='Bad:' value={props.bad} />
+      <br />
+      <Total good={props.good} neutral={props.neutral} bad={props.bad} />
+      <Average good={props.good} neutral={props.neutral} bad={props.bad} />
+      <Percentage good={props.good} neutral={props.neutral} bad={props.bad} />
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -44,14 +59,7 @@ const App = () => {
       <Button handleClick={() => setGood(good +1)} text='Good' />
       <Button handleClick={() => setNeutral(neutral + 1)} text='Neutral' />
       <Button handleClick={() => setBad(bad + 1)} text='Bad' />
-      <h1>Stats</h1>
-      <Display name='Good:' value={good} />
-      <Display name='Neutral:' value={neutral} />
-      <Display name='Bad:' value={bad} />
-      <br />
-      <Total good={good} neutral={neutral} bad={bad} />
-      <Average good={good} neutral={neutral} bad={bad} />
-      <Percentage good={good} neutral={neutral} bad={bad} />
+      <Stats good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
