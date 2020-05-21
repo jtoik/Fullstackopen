02e5dc6a542a519d2e1e5d91a6c3134e15149db0
jtoik <jeretoikkadev@gmail.com>
2020-05-21 12:28:04 +0300

    Tehtävä 1.13

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -9,22 +9,24 @@ const Button = (props) => (
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
-  var points = new Uint8Array(6)
+  const [points, setPoints] = useState([0, 0, 0, 0, 0, 0])
 
   function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
 
   function vote() {
-    points[selected] = points[selected] + 1 
+    const pointsTemp = [...points];
+    pointsTemp[selected] += 1;
+    return pointsTemp;
   }
 
   return (
     <div>
       <p>{props.anecdotes[selected]}</p>
       <p>This anecdote has {points[selected]} votes!</p>
-      <Button handleclick={() => vote()} text='Vote for this anecdote' />
-      <Button handleClick={() => setSelected(getRandomInt(6))} text='Next anecdote' />
+      <Button handleClick={() => setPoints(vote) } text='Vote for this anecdote' />
+      <Button handleClick={() => setSelected(getRandomInt(6) )} text='Next anecdote' />
     </div>
   )
 }
