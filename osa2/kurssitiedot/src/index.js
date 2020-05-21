155f222c7d032a351338334f0e64490e385e5f52
jtoik <jeretoikkadev@gmail.com>
2020-05-21 23:03:53 +0300

    Initial state of kurssitiedot

---
+++ osa2/kurssitiedot/src/index.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>
+        {props.course}
+      </h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.name} {props.exercises}
+      </p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  const parts = props.parts
+  return (
+    <div>
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const parts = props.parts
+  return (
+    <div>  
+      <p>
+        Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <>
+        <Header course={course.name} />  
+        <Content parts={course.parts} />
+        <Total parts={course.parts} />   
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
